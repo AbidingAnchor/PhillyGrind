9a0189ac08686c530ebbe318d4f06f69361e9408
AbidingAnchor <drewnegron95@gmail.com>
2026-08-19 22:33:01 -0400

    fix: add back total reaction count to ReactionBreakdown component

--- src/components/ReactionBreakdown.jsx
+++ src/components/ReactionBreakdown.jsx
@@ -36,6 +36,7 @@ export default function ReactionBreakdown({ breakdown, maxTypes = 3, className =
           </span>
         ))}
       </div>
+      <span className="reaction-summary-total">{formatReactionCount(total)}</span>
       
       {showTooltip && userReactionData && (
         <div className="reaction-tooltip">
